31ac6a432eabf301a70fc34c21d5cc316277cc49
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-04-18 13:31:41 +0330

    Add a #TODO on os.getenv

--- trader.py
+++ trader.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-API_KEY=os.getenv('nobitexToken', default=None)
+API_KEY=os.getenv('nobitexToken', default=None)    #TODO configure os.getenv to return "Token is not configured" in case of None.
 
 print(API_KEY)
