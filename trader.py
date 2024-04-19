f85d624dd1dc469433f7b4589aa5aa3dd7cbd516
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-04-19 14:42:52 +0330

    Redesign comment-tags style

--- trader.py
+++ trader.py
@@ -2,11 +2,9 @@ import os
 from dotenv import load_dotenv
 import requests
 
-
 # Load api token
 load_dotenv('.env')
-API_KEY: str | None = os.getenv('MAIN_TOKEN', default=None)  # TODO #NO-001 configure 'os.getenv' to return "Token is not configured"
-                                                             # in case of None.
+API_KEY: str | None = os.getenv('MAIN_TOKEN', default=None)  # TODO NO-001
 print(API_KEY)
 
 # Get connection to exchange
@@ -31,10 +29,18 @@ print(response2.json())
 
 SYMBOL = 'USDTIRT'
 
+
 # Function to fetch trades data
 def fetch_trades_data():
     response = requests.get(BASE_URL + f'/v2/trades/{SYMBOL}')
     response.raise_for_status()  # Raise an exception for non-2xx status codes
     return response.json()
 
+
 print(fetch_trades_data())
+
+
+"""
+TODO NO-001 configure 'os.getenv' to return "Token is not configured" in case of None.
+
+"""
