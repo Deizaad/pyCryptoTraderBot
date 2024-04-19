6df789711e45ca4392b178db234a40855cca4410
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-04-19 17:43:50 +0330

    Enhance 'comment-tags.py' with detailed data

--- comment-tags.py
+++ comment-tags.py
@@ -1,6 +1,9 @@
 """ TODO comment-tags:
 
 TODO NO-001 configure 'os.getenv' to return "Token is not configured" in case of None.
+            issue by: Deizaad -> 166605426+Deizaad@users.noreply.github.com
+                  to: myself
+                  on: 2024-04-17
 """
 ##################################################################################################
 
