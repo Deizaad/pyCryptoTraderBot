d1d7daad6fb04489ac15de0d0d146fd941700ed6
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-04-21 20:41:14 +0330

    feat(API): Add function to get OHLC data

--- trader.py
+++ trader.py
@@ -19,6 +19,67 @@ headers: dict[str, str] = {
 SYMBOL = nobitex_data.USDTIRT
 
 
+# Function to fetch and process OHLC data
+def get_ohlc_data():
+    url = nobitex_data.BASE_URL
+    endpoint = 'market/udf/history'
+
+    params = {
+        'symbol': SYMBOL,
+        'resolution': '5',
+        'from': '1562058167',
+        'to': int(nobitex_data.CURRENT_TIME),
+        'countback': '500'
+    }
+
+    try:
+        response = requests.request("GET", url + endpoint, params=params, headers=headers)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        ohlc_data = response.json()
+
+        # Extract the necessary data from the API response
+        timestamps = ohlc_data['t']
+        open_prices = ohlc_data['o']
+        high_prices = ohlc_data['h']
+        low_prices = ohlc_data['l']
+        close_prices = ohlc_data['c']
+        volumes = ohlc_data['v']
+
+        # Convert Unix timestamps to datetime objects
+        dates = [JalaliDateTime.fromtimestamp(ts) for ts in timestamps]
+
+        # Create a DataFrame with the OHLC data
+        ohlc_df = pd.DataFrame({
+            'Date': dates,
+            'Open': open_prices,
+            'High': high_prices,
+            'Low': low_prices,
+            'Close': close_prices,
+            'Volume': volumes
+        })
+
+        # Set the 'Date' column as the index
+        ohlc_df.set_index('Date', inplace=True)
+
+        return ohlc_df
+
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f'HTTP error occurred while fetching OHLC data: {http_err}')
+        logging.error(f'Response status code: {http_err.response.status_code}')
+        logging.error(f'Response reason: {http_err.response.reason}')
+        logging.error(f'Response text: {http_err.response.text}')
+
+    except Exception as err:
+        logging.error(f'Other error occurred while fetching OHLC data: {err}')
+
+    return pd.DataFrame()  # Return an empty DataFrame in case of an error
+
+
+print("       __OHLC_dataframe__", "             size: \n")
+print(get_ohlc_data())
+time.sleep(4)
+
+
 # Function to fetch and process trades data
 def get_trades_data():
     try:
@@ -59,7 +120,7 @@ def clear_console():
 
 
 # Continuously fetch and process real-time trades data
-max_iterations = 5
+max_iterations = 2
 pbar = tqdm(total=max_iterations, desc="Fetching trades data", unit="batch", colour='#660066')
 iteration = 0
 trades_df = pd.DataFrame()  # Initialize an empty DataFrame to store trades data
