34e34e301b754a245cfdbd55691b7210e745ac1f
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-06-22 13:35:56 +0330

    docs(Events): Write docstring for Event class
    Inside 'NobitexTrader/utils/event_channels.py' module.

--- NobitexTrader/utils/event_channels.py
+++ NobitexTrader/utils/event_channels.py
@@ -1,3 +1,6 @@
 class Event:
+    """
+    This class contains constant attributes for Event channels that are used in the project.
+    """
     SUCCESS_FETCH = 'successfully fetched data'
     NEW_KLINE_DATA = 'new kline data arrived'
