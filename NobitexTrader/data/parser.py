5f4f65310e2a4cbde4f92c8d0230d8f77fb210d7
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-06-24 13:35:41 +0330

    feat(data): Initiate the 'parser.py' module

---
+++ NobitexTrader/data/parser.py
@@ -0,0 +1,15 @@
+"""
+This module contains functions to parse raw data received from exchange API.
+"""
+import pandas as pd
+
+
+
+class Parser:
+    """
+    This class contains methods to parse raw data received from exchange API.
+    """
+    def kline(self, raw_kline):
+        kline_df = pd.DataFrame()
+
+        return kline_df
