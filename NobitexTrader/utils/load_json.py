2b9a99efac3a1c91af881c462ac5e9001fc1c0e9
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-06-24 13:22:13 +0330

    feat(utils): Create the 'load_json.py' module

---
+++ NobitexTrader/utils/load_json.py
@@ -0,0 +1,11 @@
+def load(file_path: str) -> dict:
+    import json
+    with open(file_path, 'r') as file:
+        config = json.load(file)
+    return config
+
+
+if __name__ == '__main__':
+    config = load(r'NobitexTrader\configs\signal_config.json')
+
+    print(config, type(config))
