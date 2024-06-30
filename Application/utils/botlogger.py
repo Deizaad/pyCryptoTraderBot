569eda6f1ae26bfa77f2536ad8f692d6c57ad9aa
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-06-30 08:28:03 +0000

    style(logs): Improve logs details and style

--- Application/utils/botlogger.py
+++ Application/utils/botlogger.py
@@ -7,21 +7,26 @@ def initialize_logger():
     file_name = str(datetime.now().strftime("%H-00")) + '.log'
 
     folder_name = './Logs/'
-    month_folder_name = str(date.today().strftime("%B_%Y-%m")) + str(JalaliDate.today().strftime("(%B_%Y-%m)/"))
-    day_folder_name = str(date.today().strftime("%A_%B_%d")) + str(JalaliDate.today().strftime("(%A_%d_%B)/"))
+    month_folder_name = str(date.today().strftime("%B_%Y-%m")) \
+                        + str(JalaliDate.today().strftime("(%B_%Y-%m)/"))
+    
+    day_folder_name = str(date.today().strftime("%A_%B_%d")) \
+                      + str(JalaliDate.today().strftime("(%A_%d_%B)/"))
     path = str(folder_name+month_folder_name+day_folder_name)
     try:
         os.makedirs(path)
     except OSError as err:
         if err.errno == 17:
-            print('Attempt to create Logs directory: It already exsist!')
+            print(f'Attempt to create Logs directory {path}: It already exsist!')
         else:
             print(f'Attempt to create Logs directory: error accured: {err}')
 
-    logging.basicConfig(filename = path+file_name,
-                        filemode='w',
-                        format = "%(levelname)s at %(asctime)s:  %(message)s",
-                        level=logging.DEBUG)
+    logging.basicConfig(
+        filename = path+file_name,
+        filemode='w',
+        format = "%(levelname)s from \"%(funcName)s\" in \"%(filename)s\" at %(asctime)s:\n    %(message)s\n",
+        level=logging.DEBUG
+    )
 
     logging.getLogger().addHandler(logging.StreamHandler())
 
