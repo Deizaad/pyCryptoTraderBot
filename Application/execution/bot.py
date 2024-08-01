8ce3dc2178280d12365eb0fdfe783b89635f38fa
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-01 09:20:39 +0000

    feat(execution): Initiate the 'bot.py' module

---
+++ Application/execution/bot.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv('project_path.env')
+path = os.getenv('PYTHONPATH')
+if path:
+    sys.path.append(path)
+
+from Application.utils.botlogger import initialize_logger  # noqa: E402
+
+
+
+initialize_logger()
