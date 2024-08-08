ba294c66fda98155d0e9dfaf947919776a703fff
Deizaad Mitra <166605426+Deizaad@users.noreply.github.com>
2024-08-08 21:40:36 +0000

    style(API): Restyle 'ApiService' class

--- Application/api/api_service.py
+++ Application/api/api_service.py
@@ -1,39 +1,36 @@
-import os
 import sys
 import httpx
 import asyncio
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv('project_path.env')
-path = os.getenv('PYTHONPATH')
-if path:
-    sys.path.append(path)
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
 
 
 
 # =================================================================================================
 class APIService:
     async def _request(self, 
-                       client: httpx.AsyncClient, 
-                       method: str, 
-                       url: str, 
-                       endpoint: str, 
-                       timeout: float,
-                       tries_interval: float,
-                       tries: int,
+                       client         : httpx.AsyncClient, 
+                       method         : str,
+                       url            : str, 
+                       endpoint       : str, 
+                       timeout        : float,
+                       tries_interval : float,
+                       tries          : int,
                        *,
-                       params=None, 
-                       data=None, 
-                       headers=None) -> dict:
+                       params         : dict[str, str] = None, 
+                       data           : dict[str, str] = None, 
+                       headers        : dict[str, str] = None) -> dict:
         
         for attempt in range(tries):
             try:
-                response = await client.request(method, 
-                                                f"{url}{endpoint}", 
-                                                params=params, 
-                                                json=data, 
-                                                headers=headers, 
-                                                timeout=timeout)
+                response = await client.request(method  = method, 
+                                                url     = f"{url}{endpoint}", 
+                                                params  = params, 
+                                                json    = data, 
+                                                headers = headers, 
+                                                timeout = timeout)
                 
                 if response.status_code == 200:
                     return response.json()
@@ -56,62 +53,81 @@ class APIService:
 
 
     async def get(self,
-                  client: httpx.AsyncClient,
-                  url: str,
-                  endpoint: str,
-                  timeout: float,
-                  tries_interval: float,
-                  tries: int,
+                  client         : httpx.AsyncClient,
+                  url            : str,
+                  endpoint       : str,
+                  timeout        : float,
+                  tries_interval : float,
+                  tries          : int,
                   *,
-                  params : dict[str, str] | None = None,
-                  data   : dict[str, str] | None = None,
-                  headers: dict[str, str] | None = None):
+                  params         : dict[str, str] | None = None,
+                  data           : dict[str, str] | None = None,
+                  headers        : dict[str, str] | None = None):
         
-        return await self._request(client=client,
-                                   method="GET",
-                                   url=url,
-                                   endpoint=endpoint,
-                                   timeout=timeout,
-                                   tries_interval=tries_interval,
-                                   tries=tries,
-                                   params=params,
-                                   data=data,
-                                   headers=headers)
+        return await self._request(client         = client,
+                                   method         = "GET",
+                                   url            = url,
+                                   endpoint       = endpoint,
+                                   timeout        = timeout,
+                                   tries_interval = tries_interval,
+                                   tries          = tries,
+                                   params         = params,
+                                   data           = data,
+                                   headers        = headers)
     # ____________________________________________________________________________ . . .
 
 
     async def post(self,
-                   client: httpx.AsyncClient,
-                   url,
-                   endpoint,
-                   timeout,
-                   interval,
-                   tries,
+                   client         : httpx.AsyncClient,
+                   url            : str,
+                   endpoint       : str,
+                   timeout        : float,
+                   tries_interval : float,
+                   tries          : int,
                    *,
-                   data,
-                   headers):
+                   data           : dict[str, str],
+                   headers        : dict[str, str]):
+        
+        return await self._request(client         = client,
+                                   method         = "POST",
+                                   url            = url,
+                                   endpoint       = endpoint,
+                                   timeout        = timeout,
+                                   tries_interval = tries_interval,
+                                   tries          = tries,
+                                   data           = data,
+                                   headers        = headers)
+    # ____________________________________________________________________________ . . .
+
+
+    async def put(self,
+                  client         : httpx.AsyncClient,
+                  url            : str,
+                  endpoint       : str,
+                  timeout        : float,
+                  tries_interval : float,
+                  tries          : int,
+                  *,
+                  data           : dict[str, str] = None):
         
         return await self._request(
-            client, "POST", url, endpoint, timeout, interval, tries, data=data, headers=headers
+            client, "POST", url, endpoint, timeout, tries_interval, tries, data=data
             )
     # ____________________________________________________________________________ . . .
 
 
-    async def put(
-            self, client: httpx.AsyncClient, url, endpoint, timeout, interval, tries, *, data=None
+    async def delete(self,
+                     client         : httpx.AsyncClient,
+                     url            : str,
+                     endpoint       : str,
+                     timeout        : float,
+                     tries_interval : float,
+                     tries          : int,
+                     *,
+                     data           : dict[str, str]=None
             ):
         
         return await self._request(
-            client, "POST", url, endpoint, timeout, interval, tries, data=data
-            )
-    # ____________________________________________________________________________ . . .
-
-
-    async def delete(
-            self, client: httpx.AsyncClient, url, endpoint, timeout, interval, tries, *, data=None
-            ):
-        
-        return await self._request(
-            client, "POST", url, endpoint, timeout, interval, tries, data=data
+            client, "POST", url, endpoint, timeout, tries_interval, tries, data=data
             )
 # =================================================================================================
