222884817f3476c13ea9107212b635b297a2a645
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-08 15:20:28 +0000

    feat:Attach start_fetching_kline to start_activity

--- Application/execution/profiles/base_live.py
+++ Application/execution/profiles/base_live.py
@@ -5,11 +5,13 @@ path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
 from Application.utils.event_channels import Event    # noqa: E402
+from Application.data.data_processor import DataProcessor    # noqa: E402
 from Application.trading.trade_engine import start_trade_engine    # noqa: E402
 from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
 from Application.execution.scheduler import register_activity_events, watch_transitions    # noqa: E402
 
 jarchi = EventHandler()
+data   = DataProcessor()
 
 
 
@@ -17,8 +19,13 @@ async def run():
     """
     
     """
+    # Register events
     register_activity_events()
+    
+    # Attach listeners to events
     jarchi.attach(start_trade_engine, Event.START_ACTIVITY)
+    jarchi.attach(data.start_fetching_kline, Event.START_ACTIVITY)
 
-    watch_transitions()
+    # Schedule activity
+    await watch_transitions()
 # ________________________________________________________________________________ . . .
