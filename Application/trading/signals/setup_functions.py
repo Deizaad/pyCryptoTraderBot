5352c59b13242536de2ec617d53c46f716bf846f
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-12 16:29:07 +0000

    remove(setups): Omit 'get_selected_setups()'
    Omitted this function from 'setup_functions.py'

--- Application/trading/signals/setup_functions.py
+++ Application/trading/signals/setup_functions.py
@@ -1,80 +1,9 @@
-import os
-import sys
 import logging
-import importlib
 import pandas as pd
-from dotenv import load_dotenv
-
-load_dotenv('project_path.env')
-path = os.getenv('PYTHONPATH')
-if path:
-    sys.path.append(path)
-
-from Application.configs import config as cfg    # noqa: E402
-from Application.trading.analysis.indicator_classes import Supertrend    # noqa: E402
 
 
 
 # =================================================================================================
-def get_selected_setups(module_path: str, config: dict) -> tuple[dict, dict]:
-    """
-    This function returns strategy setups from 'setup_functions' module that are selected in config
-    file along with their properties.
-    
-    Parameters:
-        module_path (str): Path to the module where setup functions are located.
-        config (dict): A pre-loaded .json configuration file.
-    
-    Returns:
-        tuple: A tuple containing two dictionaries:
-            - selected_functions: Dictionary of selected setup functions and their properties.
-            - required_indicators: Dictionary of required indicators and their parameters.
-    """
-    setups_module = importlib.import_module(module_path)
-    setup_functions = {
-        name: func for name, func in setups_module.__dict__.items()
-        if callable(func) and hasattr(func, 'required_indicators')
-    }
-
-    selected_setups = config['setups']
-    
-    selected_functions = {}
-    required_indicators = {}
-
-    try:
-        for setup in selected_setups:
-            setup_name = setup['name']
-            if setup_name in setup_functions:
-                func = setup_functions[setup_name]
-                selected_functions[setup_name] = {
-                    'function': setup_functions[setup_name],
-                    'properties': setup['properties']
-                }
-                required_indicators[setup_name] = func.required_indicators
-            else:
-                raise NameError(f'Setup "{setup_name}" from config file could not be found in' \
-                                 'defined setup functions. Perhaps you meisspell it?')
-
-        return selected_functions, required_indicators
-    except NameError as err:
-        logging.error(err)
-        return selected_functions, required_indicators
-# =================================================================================================
-
-
-
-# =================================================================================================
-def requires_indicators(*indicators):
-    def decorator(func):
-        func.required_indicators = indicators
-        return func
-    return decorator
-# =================================================================================================
-
-
-
-# =================================================================================================
-@requires_indicators(Supertrend(cfg.Study.Supertrend.WINDOW, cfg.Study.Supertrend.FACTOR))
 async def supertrend_setupfunc(
     kline_df: pd.DataFrame, indicator_df: pd.DataFrame, properties: dict
 ) -> pd.DataFrame:
@@ -111,18 +40,3 @@ async def supertrend_setupfunc(
 
 # Definition of other setup functions ...
 # =================================================================================================
-
-
-if __name__ == '__main__':
-    selected, indicators = get_selected_setups('Application.trading.signals.setup_functions', {
-        "setups": [
-            {
-                "name": "supertrend_setupfunc",
-                "properties": {
-                    "window": 14,
-                    "factor": 3
-                }
-            }
-        ]
-        })
-    print(selected, '\n', indicators)
