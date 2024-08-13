2771854defb9ce5acb54153657305c7fff07499f
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-13 16:39:30 +0000

    feat(events): Add two trade related event_channels

--- Application/utils/event_channels.py
+++ Application/utils/event_channels.py
@@ -2,11 +2,13 @@ class Event:
     """
     This class contains constant attributes for Event channels that are used in the project.
     """
-    START_ACTIVITY  = 'active time is started'
-    END_ACTIVITY    = 'active time is ended'
-    SUCCESS_FETCH   = 'successfully fetched data'
-    NEW_KLINE_DATA  = 'new kline data arrived'
-    MARKET_IS_VALID = 'market is valid'
-    OPEN_POSITIONS_EXIST    = 'there are open positions available'
+    START_ACTIVITY    =  'active time is started'
+    END_ACTIVITY      = 'active time is ended'
+    SUCCESS_FETCH     = 'successfully fetched data'
+    NEW_KLINE_DATA    = 'new kline data arrived'
+    MARKET_IS_VALID   = 'market is valid'
+    TRADE_GOT_CLOSED  = 'current trade got closed'
+    THERE_IS_NO_TRADE = 'there is no open trade'
+    OPEN_POSITIONS_EXIST            = 'there are open positions available'
     RECOVERY_MECHANISM_ACCOMPLISHED = 'recovery mechanism is done successfully'
     NEW_VALIDATION_INDICATOR_DATA   = 'there are new data available for validation indicators'
