f916817242c402f74ea83f4b343e90222de1e620
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-14 14:18:57 +0000

    ref(signals): Move definition of 'ENTRY_SYSTEM'
    Moved the definition of this constant from 'signals_chief.py' to
    'signal_supervisor.py'.

--- Application/trading/signals/signal_supervisor.py
+++ Application/trading/signals/signal_supervisor.py
@@ -1,7 +1,22 @@
+import sys
 import asyncio
 import logging
 import pandas as pd
+from dotenv import dotenv_values
 
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
+
+from Application.utils.load_json import load    # noqa: E402
+from Application.data.data_tools import extract_strategy_fields_functions    # noqa: E402
+
+# Extract trading system functions
+ENTRY_SYSTEM: list  = extract_strategy_fields_functions(
+    field                           = 'entry_signal_setups',
+    config                          = load(r'Application/configs/strategy.json'),
+    setup_functions_module_path     = 'Application.trading.signals.setup_functions',
+    indicator_functions_module_path = 'Application.trading.analysis.indicator_functions'
+)
 
 
 async def generate_signals(trading_system : list,
@@ -37,99 +52,6 @@ async def generate_signals(trading_system : list,
     return signal_df
 # ________________________________________________________________________________ . . .
 
-
-# def declare_setups(category: str, path_to_setups_module: str, configs: Dict[str, Any]):
-#     """
-#     Declares chosen strategy setups and required indicators from config file.
-
-#     Parameters:
-#         category (str): Category of setup. Expects eather 'entry_signal_setups' | 'take_profit_setups' | 'stop_loss_setups'.
-#         path_to_setups_module (str): The file path to the setups module where strategy setups are defined.
-#         configs (Dict[str, Any]): Preloaded configuration dictionary that includes the selected setups and their properties.
-    
-#     Returns:
-#         Tuple (Dict[Callable, Dict[str, Any]], Set):
-#             - A dictionary mapping setup functions to their properties.
-#             - A set of required indicators.
-#     """
-#     declared_indicators: Set[tuple] = set()
-#     declared_setup_functions: Dict[Callable, Dict[str, float]] = {}
-
-#     selected_setups, required_indicators = get_selected_setups(setup_type  = category,
-#                                                                module_path = path_to_setups_module,
-#                                                                config      = configs)
-    
-#     # Extract setup functions
-#     for setup in selected_setups:
-#         setup_func: Callable = selected_setups[setup]['function']
-#         properties: dict     = selected_setups[setup]['properties']
-
-#         declared_setup_functions[setup_func] = properties
-#         logging.info(f'Setup "{setup}" has been added to "declared_setup_functions".')
-
-#     # Extract indicators
-#     for value in required_indicators.values():
-#             for item in value:
-#                 if item not in declared_indicators:
-#                     declared_indicators.add(item)
-#                     logging.info(f'Indicator {item} has been added to "declared_indicators".')
-
-#     return declared_setup_functions, declared_indicators
-# # ________________________________________________________________________________ . . .
-
-
-# # =================================================================================================
-
-
-
-# # =================================================================================================
-# class SignalChief:
-#     def declare_setups(self, path_to_setups_module: str, configs: dict):
-#         """
-#         Declares chosen strategy setups.
-#         """
-#         self.declared_setup_functions: dict[Callable, dict[str, float]] = {}
-
-#         selected_setups, _ = get_selected_setups('entry_signal_setups', path_to_setups_module, configs)
-
-#         for setup in selected_setups:
-#             setup_func: Callable = selected_setups[setup]['function']
-#             properties: dict     = selected_setups[setup]['properties']
-
-#             self.declared_setup_functions[setup_func] = properties
-
-#             logging.info(f'Setup "{setup}" has been added to "declared_setup_functions".')
-#     # ____________________________________________________________________________ . . .
-
-
-#     async def generate_signals(self, kline_df: pd.DataFrame, indicator_df: pd.DataFrame):
-#         """
-#         Executes setup functions asynchronously to return generated signals as a dataframe.
-#         """
-#         tasks = []
-#         for function in self.declared_setup_functions:
-#             tasks.append(function(kline_df=kline_df,
-#                                   indicator_df=indicator_df,
-#                                   properties=self.declared_setup_functions[function]))
-
-#         try:
-#             results: list[pd.DataFrame] = await asyncio.gather(*tasks)
-#         except asyncio.CancelledError as err:
-#             logging.error(f"A task got cancelled in generate_signals() of SignalChief: {err}")
-#             raise
-#         except Exception as err:
-#             logging.error(f'Inside generate_signals() method of SignalChief: {err}')
-
-#         signal_df = pd.DataFrame(index=kline_df.index)
-
-#         for result in results:
-#             if not result.empty:
-#                 signal_df = signal_df.merge(
-#                     result, left_index=True, right_index=True, how='left'
-#                 )
-
-#         return signal_df
-# =================================================================================================
 
 
 
--- Application/trading/signals/signals_chief.py
+++ Application/trading/signals/signals_chief.py
@@ -1,32 +1,16 @@
 import sys
-from functools import partial
 from dotenv import dotenv_values
 
 path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
-from Application.utils.load_json import load    # noqa: E402
 from Application.utils.event_channels import Event    # noqa: E402
 from Application.data.data_processor import DataProcessor    # noqa: E402
-from Application.trading.market.validator import MarketValidator    # noqa: E402
 from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
-from Application.data.data_tools import extract_strategy_fields_functions    # noqa: E402
+from Application.trading.market.validation_chief import start_market_validation    # noqa: E402
 
 data = DataProcessor()
 jarchi = EventHandler()
-market_validator = MarketValidator()
-
-
-# Extract trading system functions
-ENTRY_SYSTEM: list  = extract_strategy_fields_functions(
-    field                           = 'entry_signal_setups',
-    config                          = load(r'Application/configs/strategy.json'),
-    setup_functions_module_path     = 'Application.trading.signals.setup_functions',
-    indicator_functions_module_path = 'Application.trading.analysis.indicator_functions'
-)
-
-PARTIAL_generating_signals = partial(data.generating_signals, trading_system=ENTRY_SYSTEM)
-PARTIAL_computing_indicators = partial(data.computing_indicators, trading_system=ENTRY_SYSTEM)
 
 
 
@@ -64,8 +48,8 @@ async def post_market_validation_flow():
     """
     Performing jobs on "MARKET_IS_VALID" event.
     """
-    jarchi.attach(PARTIAL_computing_indicators, Event.NEW_KLINE_DATA)
-    jarchi.attach(PARTIAL_generating_signals, Event.NEW_INDICATORS_DATA)
+    jarchi.attach(data.computing_indicators, Event.NEW_KLINE_DATA)
+    jarchi.attach(data.generating_signals, Event.NEW_INDICATORS_DATA)
 # ________________________________________________________________________________ . . .
 
 
@@ -73,9 +57,9 @@ async def pre_market_validation_flow():
     """
     Performs jobs on "there_is_no_open_trade" event.
     """
-    await market_validator.start_market_validation()
+    await start_market_validation()
 
-    jarchi.detach(PARTIAL_generating_signals, Event.NEW_INDICATORS_DATA)
-    jarchi.detach(PARTIAL_computing_indicators, Event.NEW_KLINE_DATA)
+    jarchi.detach(data.generating_signals, Event.NEW_INDICATORS_DATA)
+    jarchi.detach(data.computing_indicators, Event.NEW_KLINE_DATA)
 # ________________________________________________________________________________ . . .
 # =================================================================================================
