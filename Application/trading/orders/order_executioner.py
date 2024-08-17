10760024cd655a88f992f3cbd8039632908a879d
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-17 15:35:08 +0000

    feat(orders): Initiate 'order_executioner.py'

---
+++ Application/trading/orders/order_executioner.py
@@ -0,0 +1,36 @@
+
+
+
+ENTRY_ORDERING_SETUP       : dict = {}
+TAKE_PROFIT_ORDERING_SETUP : dict = {}
+STOP_LOSS_ORDERING_SETUP   : dict = {}
+COMBO_TP_SL_ORDERING_SETUP : dict = {}
+
+
+
+async def trade_entry_executioner():
+    """
+    
+    """
+    pass
+
+
+async def take_profit_executioner():
+    """
+    
+    """
+    pass
+
+
+async def stop_loss_executioner():
+    """
+    
+    """
+    pass
+
+
+async def combo_tp_sl_executioner():
+    """
+    
+    """
+    pass
