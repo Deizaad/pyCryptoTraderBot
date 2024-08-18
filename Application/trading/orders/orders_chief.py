f79d807a7a329761d36b06f9f29e95a271be23e9
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-18 15:43:39 +0000

    stage(orders): Stage works on 'orders_chief.py'

--- Application/trading/orders/orders_chief.py
+++ Application/trading/orders/orders_chief.py
@@ -1,5 +1,28 @@
+import sys
+from dotenv import dotenv_values
+
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
+
+from Application.utils.event_channels import Event    # noqa: E402
+from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
+from Application.trading.orders.order_executioner import trade_entry_executioner    # noqa: E402
+
+jarchi = EventHandler()
+
+
+
 async def start_orders_engine():
     """
     Starts the orders engine.
+    """
+    # Attach 'trade_entry_executioner' to 'new_valid_entry_signal' event.
+    jarchi.attach(trade_entry_executioner, Event.)
+
+
+
+async def orders_state_machine():
+    """
+    Manages state of orders.
     """
     pass
