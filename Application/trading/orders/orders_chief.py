3bbaa1a4fcff4a400eec30b94081028cde8e81f6
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-19 16:06:07 +0000

    feat(orders): Attach 'order_plecement_functions'

--- Application/trading/orders/orders_chief.py
+++ Application/trading/orders/orders_chief.py
@@ -6,7 +6,10 @@ sys.path.append(path) if path else None
 
 from Application.utils.event_channels import Event    # noqa: E402
 from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
-from Application.trading.orders.order_executioner import trade_entry_executioner    # noqa: E402
+from Application.trading.orders.order_executioner import stop_loss_executioner,\
+                                                         trade_entry_executioner,\
+                                                         combo_tp_sl_executioner,\
+                                                         take_profit_executioner    # noqa: E402
 
 jarchi = EventHandler()
 
@@ -16,8 +19,10 @@ async def start_orders_engine():
     """
     Starts the orders engine.
     """
-    # Attach 'trade_entry_executioner' to 'new_valid_entry_signal' event.
-    jarchi.attach(trade_entry_executioner, Event.)
+    jarchi.attach(stop_loss_executioner, Event.VALID_SL_SIGNAL)
+    jarchi.attach(take_profit_executioner, Event.VALID_TP_SIGNAL)
+    jarchi.attach(combo_tp_sl_executioner, Event.VALID_TP_SL_SIGNAL)
+    jarchi.attach(trade_entry_executioner, Event.VALID_ENTRY_SIGNAL)
 
 
 
