9f29b5aeddd606c7aeb52116bd57b633c01d7e3b
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-19 16:07:36 +0000

    feat(orders): Define ordering setup constants

--- Application/trading/orders/order_executioner.py
+++ Application/trading/orders/order_executioner.py
@@ -1,10 +1,33 @@
+import sys
+from dotenv import dotenv_values
+
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
+
+from Application.utils.load_json import load    # noqa: E402
+from Application.data.data_tools import extract_strategy_fields_functions    # noqa: E402
 
 
-
-ENTRY_ORDERING_SETUP       : dict = {}
-TAKE_PROFIT_ORDERING_SETUP : dict = {}
-STOP_LOSS_ORDERING_SETUP   : dict = {}
-COMBO_TP_SL_ORDERING_SETUP : dict = {}
+ENTRY_ORDERING_SETUP       = extract_strategy_fields_functions(
+    field                           = 'entry_order_placement_setup',
+    config                          = load(r'Application/configs/strategy.json'),
+    setup_functions_module_path     = 'Application.trading.orders.ordering_functions'
+)
+TAKE_PROFIT_ORDERING_SETUP = extract_strategy_fields_functions(
+    field                           = 'take_profit_order_placement_setup',
+    config                          = load(r'Application/configs/strategy.json'),
+    setup_functions_module_path     = 'Application.trading.orders.ordering_functions'
+)
+STOP_LOSS_ORDERING_SETUP   = extract_strategy_fields_functions(
+    field                           = 'stop_loss_order_placement_setup',
+    config                          = load(r'Application/configs/strategy.json'),
+    setup_functions_module_path     = 'Application.trading.orders.ordering_functions'
+)
+COMBO_TP_SL_ORDERING_SETUP = extract_strategy_fields_functions(
+    field                           = 'combo_of_tp_sl_order_placement_setup',
+    config                          = load(r'Application/configs/strategy.json'),
+    setup_functions_module_path     = 'Application.trading.orders.ordering_functions'
+)
 
 
 
