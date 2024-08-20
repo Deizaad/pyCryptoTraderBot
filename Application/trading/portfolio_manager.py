25ed58c82cd809c193a53d0bc22621bebdf82365
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-20 14:45:05 +0000

    feat(trading): Implement 'fetch_portfolio_balance'

---
+++ Application/trading/portfolio_manager.py
@@ -0,0 +1,51 @@
+import sys
+import httpx
+import asyncio
+from dotenv import dotenv_values
+
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
+
+from Application.data.user import User             # noqa: E402
+from Application.api.nobitex_api import Market     # noqa: E402
+from Application.api.nobitex_api import Account    # noqa: E402
+from Application.api.api_service import APIService # noqa: E402
+
+account = Account()
+market = Market(APIService(), httpx.AsyncClient())
+
+
+
+# =================================================================================================
+async def fetch_portfolio_balance() -> tuple[int, float]:
+    """
+    Fetches the portfolio balance of user.
+
+    Parameters:
+        user_token (str): User's API token.
+
+    Returns:
+        porfolio_balance (tuple): A tuple containing portfolio balance in 'Rials' and 'USD'.
+    """
+    async with httpx.AsyncClient() as http_client:
+        wallets_coroutine = account.wallets(http_agent = http_client,
+                                            token      = User.TOKEN,    # type: ignore
+                                            drop_void  = True)
+
+        price_rate_coroutine = market.fetch_market_price(http_agent  = http_client,
+                                                        src_currency = 'usdt',
+                                                        dst_currency = 'rls')
+
+        wallets_df, usdt_rate = await asyncio.gather(wallets_coroutine, price_rate_coroutine)
+
+    portfolio_balance_rials = wallets_df.rial_balance.sum()
+    portfolio_balance_usd = round((portfolio_balance_rials / usdt_rate), 2)
+
+    return portfolio_balance_rials, portfolio_balance_usd
+# =================================================================================================
+
+
+
+if __name__ == '__main__':
+    rls, usd = asyncio.run(fetch_portfolio_balance())
+    print(rls, usd)
