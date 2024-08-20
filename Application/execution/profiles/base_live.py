15ce74134105dd12d107dd44ca88874706d50f2b
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-20 15:36:09 +0000

    ref(profile): Move attach signal_engine to profile

--- Application/execution/profiles/base_live.py
+++ Application/execution/profiles/base_live.py
@@ -4,11 +4,13 @@ from dotenv import dotenv_values
 path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
-from Application.utils.event_channels import Event                     # noqa: E402
-from Application.data.data_processor import DataProcessor              # noqa: E402
-from Application.execution.scheduler import watch_transitions          # noqa: E402
-from Application.trading.trade_engine import start_trade_engine        # noqa: E402
-from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
+from Application.utils.event_channels import Event                                                # noqa: E402
+from Application.data.data_processor import DataProcessor                                         # noqa: E402
+from Application.execution.scheduler import watch_transitions                                     # noqa: E402
+from Application.trading.trade_engine import start_trade_engine                                   # noqa: E402
+from Application.utils.simplified_event_handler import EventHandler                               # noqa: E402
+from Application.trading.trading_workflow import start_live_trading_flow, TRADING_APPROACH_CONFIG # noqa: E402
+from Application.trading.signals.signals_chief import start_signals_engine #, stop_signals_engine # noqa: E402
 
 data = DataProcessor()
 jarchi = EventHandler()
@@ -22,6 +24,8 @@ async def run():
     """
     _attach_to_events()
     await watch_transitions()
+
+    start_live_trading_flow(TRADING_APPROACH_CONFIG)
 # ________________________________________________________________________________ . . .
 
 
@@ -29,6 +33,9 @@ def _attach_to_events():
     """
     Attaches listeners to their corresponding event channels.
     """
+    # listeners of RECOVERY_MECHANISM_ACCOMPLISHED accomplished event channel
+    jarchi.attach(start_signals_engine, Event.RECOVERY_MECHANISM_ACCOMPLISHED)
+
     # listeners of START_ACTIVITY event channel
     jarchi.attach(start_trade_engine, Event.START_ACTIVITY)
     jarchi.attach(data.start_fetching_kline, Event.START_ACTIVITY)
--- Application/trading/trade_engine.py
+++ Application/trading/trade_engine.py
@@ -4,12 +4,7 @@ from dotenv import dotenv_values
 path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
-from Application.utils.event_channels import Event    # noqa: E402
-from Application.utils.simplified_event_handler import EventHandler    # noqa: E402
 from Application.execution.actions.disaster_actions import recovery_mechanism    # noqa: E402
-from Application.trading.signals.signals_chief import start_signals_engine #, stop_signals_engine    # noqa: E402
-
-jarchi = EventHandler()
 
 
 
@@ -18,8 +13,6 @@ async def start_trade_engine():
     """
     Starts trade engine.
     """
-    jarchi.attach(start_signals_engine, Event.RECOVERY_MECHANISM_ACCOMPLISHED)
-
     await recovery_mechanism()
 # ________________________________________________________________________________ . . .
 
