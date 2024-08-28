49b90a1b5364a96d2c070f56cb2e6827ce0cc541
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-28 17:05:17 +0000

    feat(stop_loss): Initiate 'stop_loss/stop_loss.py'

---
+++ Application/trading/stop_loss/stop_loss.py
@@ -0,0 +1,19 @@
+def trailing_order():
+    """
+    
+    """
+    pass
+
+
+def place_static_stop_loss():
+    """
+    
+    """
+    pass
+
+
+def declare_static_sl_price():
+    """
+    
+    """
+    pass
