9f2c4fa5f1c664ad95162b93a9f986c45d0447b7
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-29 11:05:40 +0000

    feat(sl): Impelement 'declare_static_sl_price()'
    Impelemented this function in 'trading/stop_loss/stop_loss.py' module.

--- Application/trading/stop_loss/stop_loss.py
+++ Application/trading/stop_loss/stop_loss.py
@@ -1,3 +1,13 @@
+import sys
+from typing import Callable
+from dotenv import dotenv_values
+
+path = dotenv_values('project_path.env').get('PYTHONPATH')
+sys.path.append(path) if path else None
+
+from Application.trading.strategy_fields import STATIC_SL_APPROACH # noqa: E402
+
+
 def trailing_order():
     """
     
@@ -12,8 +22,12 @@ def place_static_stop_loss():
     pass
 
 
-def declare_static_sl_price():
+def declare_static_sl_price(trade_side: str):
     """
     
     """
-    pass
+    func: Callable = STATIC_SL_APPROACH['function']
+    properties = STATIC_SL_APPROACH['properties']
+
+    stop_loss_price = func(trade_side=trade_side, **properties)
+    return stop_loss_price
