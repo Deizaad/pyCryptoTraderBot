106b05238caec89b4314184e27864685dd424cb3
Deizaad <166605426+Deizaad@users.noreply.github.com>
2024-08-30 16:01:51 +0000

    stage(trading): Stage works trading_flow_funcs.py

--- Application/trading/trading_approach_functions.py
+++ Application/trading/trading_approach_functions.py
@@ -6,6 +6,7 @@ path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
 from Application.utils.event_channels import Event                                          # noqa: E402
+from Application.data.exchange import Nobitex as nb                                         # noqa: E402
 from Application.data.data_processor import DataProcessor                                   # noqa: E402
 from Application.trading import strategy_fields as strategy                                 # noqa: E402
 from Application.utils.simplified_event_handler import EventHandler                         # noqa: E402
@@ -25,11 +26,13 @@ async def approach_01(properties):
     This approach enters into trades via a limit 
     """
     # perform position_sizing
-    await compute_position_margin_size(portfolio_balance  = data.get_portfolio_balance(),
-                                       risk_per_trade_pct = strategy.RISK_PER_TRADE,
-                                       entry_price        = data.get_market_price(),
-                                       stop_loss_price    = data.get_next_trade().at[0, 'init_sl'],
-                                       slippage_pct       = )
+    position_size_without_slippage = await compute_position_margin_size(
+        portfolio_balance  = data.get_portfolio_balance(),          # MOVE THE COPUTION OF POSITION SIZE TO A PROPER PLACE AS HERE IS JUST TO DECLARE THE WORKFLOW
+        risk_per_trade_pct = strategy.RISK_PER_TRADE,
+        entry_price        = data.get_market_price(),
+        stop_loss_price    = data.get_next_trade().at[0, 'init_sl'],
+        maker_fee          =
+    )
 
     # # attach listeners to NEW_VALID_SIGNAL
     # jarchi.attach(trade_entry_executioner, Event.VALID_ENTRY_SIGNAL)
