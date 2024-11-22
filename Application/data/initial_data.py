d2a52bd3ac4b398429933ff458c12c197e66c7ab
Deizaad Mitra <166605426+Deizaad@users.noreply.github.com>
2024-11-22 10:20:44 +0000

    fix(data): Solve circular import 'initial_data.py'

--- Application/data/initial_data.py
+++ Application/data/initial_data.py
@@ -5,11 +5,11 @@ from dotenv import dotenv_values
 path = dotenv_values('project_path.env').get('PYTHONPATH')
 sys.path.append(path) if path else None
 
-from Application.data.data_tools import extract_field_value # noqa: E402
+from Application.utils.load_json import load # noqa: E402
 
 
 
-LOCAL_TIME_ZONE_NAME = extract_field_value(field       = 'local_time_zone_name',
-                                           config_path = r'Application/configs/config.json')
 
-LOCAL_TIME_ZONE = ZoneInfo(LOCAL_TIME_ZONE_NAME)
+
+LOCAL_TIME_ZONE_NAME = load(r'Application/configs/config.json').get('local_time_zone_name')
+LOCAL_TIME_ZONE      = ZoneInfo(LOCAL_TIME_ZONE_NAME)
